b42ad63eb8296ec8e34bed87171e74cb800ea3dd
Tommy <tdominguezpalacio@gmail.com>
2026-06-18 12:12:45 -0500

    Update server port and improve logging

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import os
-
-print("PYTHON STARTED")
-
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
-HOST = "0.0.0.0"
-PORT = int(os.environ.get("PORT", 8000))
+PORT = int(os.environ.get("PORT", 8080))
 
 class AppHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print("REQUEST:", self.path)
+
         self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
         self.end_headers()
         self.wfile.write(b"HELLO FROM RAILWAY")
 
-server = ThreadingHTTPServer((HOST, PORT), AppHandler)
-
-print("SERVER STARTING ON", PORT)
-
+print("STARTING")
+server = ThreadingHTTPServer(("0.0.0.0", PORT), AppHandler)
+print("LISTENING ON", PORT)
 server.serve_forever()
-
 
 
 
